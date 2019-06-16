d00c5733a152ced527ceddea0e27592cbfc2a8a6
GSriyarsha <48006019+GSriyarsha@users.noreply.github.com>
2019-06-16 17:18:26 +0530

    Create PrimeNumbers.cpp

---
+++ PrimeNumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+public class PrimeNumbers
+{
+  void main()
+  {
+    int N;
+    cin >> N;
+    if ( (N & 1) == 1){
+      cout << "true";
+    }
+    else{
+      cout << "false";
+    }
+  }
+}
